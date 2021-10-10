57e2f45b6816c3d0c0f20b8e9c635b6490cd1871
Mohammed_Ismaiel <mohammedismaiel333.z@gmail.com>
2021-10-11 00:24:15 +0200

    v8

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,26 +1,8 @@
-export const config = {
-  'username': process.env.POSTGRES_USERNAME,
-  'password':`${process.env.POSTGRES_PASSWORD}`,
-  'database': process.env.POSTGRES_DB,
-  'host': process.env.POSTGRES_HOST,
-  'dialect': 'postgres',
-  'aws_region': process.env.AWS_REGION,
-  'aws_profile': process.env.AWS_PROFILE,
-  'aws_media_bucket': process.env.AWS_BUCKET,
-  'url': process.env.URL,
-  'jwt': {
-    'secret': process.env.JWT_SECRET,
-  },
-};
-
-/*
-  local variables
-*/
 // export const config = {
-//   'username': 'postgres',
-//   'password': 'postgres',
-//   'database': 'localhost',
-//   'host': 5432,
+//   'username': process.env.POSTGRES_USERNAME,
+//   'password':`${process.env.POSTGRES_PASSWORD}`,
+//   'database': process.env.POSTGRES_DB,
+//   'host': process.env.POSTGRES_HOST,
 //   'dialect': 'postgres',
 //   'aws_region': process.env.AWS_REGION,
 //   'aws_profile': process.env.AWS_PROFILE,
@@ -30,3 +12,21 @@ export const config = {
 //     'secret': process.env.JWT_SECRET,
 //   },
 // };
+
+/*
+  local variables
+*/
+export const config = {
+  'username': 'mohammedismaiel',
+  'password': 'mohammedismaiel',
+  'database': 'mohammedismaiel',
+  'host': "database-1.ctpvdajc29u8.eu-west-2.rds.amazonaws.com",
+  'dialect': 'postgres',
+  'aws_region': "eu-west-2",
+  'aws_profile': "default",
+  'aws_media_bucket': "udagram-micro-bucket",
+  'url': process.env.URL,
+  'jwt': {
+    'secret': "my-JWT-secret",
+  },
+};
